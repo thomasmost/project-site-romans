7e3360980fef6560d0c0703c87ec27b6d9160f11
tcm_NIMBUS <tomconmo@gmail.com>
2020-05-29 01:03:00 -0400

    added AHAP workshop to production history

--- src/pages/production-history.tsx
+++ src/pages/production-history.tsx
@@ -12,6 +12,23 @@ const ProductionHistoryPage = () => (
     <h1>Production History</h1>
     <p><em>"Well, you know what they say... 'all roads lead to...'"</em></p>
     <p><em>"...what? All roads lead to what?"</em></p>
+    <h3>May 14th, 2020 - Remote Workshop with At Home Artists Project</h3>
+    <p>
+       In the midst of quarantine, <a href="https://www.athomeartistsproject.com/">At Home Artists Project</a> produced a remote workshop directed by <a href="http://asiagagnon.virb.com/">Asia Gagnon</a>, rewrittend and retooled specifically for the 'webinar' format.
+    </p>
+    <ArchImage />
+    <ul className={style.castList}>
+       <li>
+         Romulus - <a href="https://www.imdb.com/name/nm6928501/">Brian Morabito</a>
+       </li>
+       <li>
+         Remus - <a href="https://twitter.com/thomascmost">Thomas Constantine Moore</a>
+       </li>
+       <li>
+         Hersilia - <a href="https://www.alexisgfloyd.com/">Alexis Floyd</a>
+       </li>
+    </ul>
+    <hr/>
     <h3>December 14th, 2018 - Immersive Reading at Arts on Site</h3>
     <p>
        In 2018, <a href="https://www.facebook.com/experiencerebis/">Rebis Immersive</a> produced a script-in-hand, one-night-only performance of
